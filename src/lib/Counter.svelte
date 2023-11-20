e1ac5c3d1287ce44dff787bd5cfaa2fe50c94320
Tom Wilson <tom@hyper.io>
2023-11-20 13:46:02 +0000

    update demo

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,12 +1,20 @@
 <script>
   import Stamps from "@permaweb/stampjs";
   import { WarpFactory } from "https://unpkg.com/warp-contracts/bundles/web.bundle.min.js";
+  import Arweave from "arweave";
 
+  const arweave = Arweave.init({
+    host: "arweave.net",
+    port: 443,
+    protocol: "https",
+  });
   let count = 0;
-  const stamps = Stamps.init({ warp: WarpFactory.forMainnet() });
-  const asset = "mKxjgkhXjqbK_kPWuUdJrkgus57qjEM63rp3_-8LE_E";
-  stamps.count(asset).then((result) => (count = result.total));
-
+  const stamps = Stamps.init({ warp: WarpFactory.forMainnet(), arweave });
+  const asset = "fsxlnMB_OHQJJwnd-2uuGNOr9JrLRhyEnBPnIxHgzII";
+  //stamps.count(asset).then((result) => (count = result.total));
+  stamps.counts([asset]).then((results) => {
+    console.log(results);
+  });
   const increment = async () => {
     const result = await stamps.stamp(asset);
     console.log(result);
